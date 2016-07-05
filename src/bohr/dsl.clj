0cf2736bfe01f509c1ad92e21922f1c0cbf0100f
Dhruv Bansal <shrub.vandal@gmail.com>
2016-07-05 23:50:42 +0000

    No need for static macro.

--- src/bohr/dsl.clj
+++ src/bohr/dsl.clj
@@ -9,29 +9,14 @@
 
 (defn- extract-observer-arguments
   "Helper function for extracting the arguments to pass to the
-  `define-observer!` function from within the `static` and `observe`
-  macros defined below."
+  `define-observer!` function from within the `observe` macro defined
+  below."
   [macro-form]
   (let [macro-vec   (apply vector macro-form)
         name        (nth macro-vec 1)
         options-vec (subvec macro-vec 2 (count macro-vec))
         options     (apply hash-map options-vec)]
     (vector name options)))
-
-(defmacro static
-  "Define an observer whose reading never expires.
-
-  A 'static' observer will make its reading when the script is first
-  loaded, prior to Bohr requesting more general observers to make
-  their first readings."
-  [name value]
-  (let [[name options]
-        (extract-observer-arguments (butlast &form))]
-    `(let [initial-value# ~(last &form)]
-       (define-observer!
-         ~name
-         ~options
-         (fn [] initial-value#)))))
 
 (defmacro observe
   "Define an observer."
