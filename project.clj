c340e48b4f48e3873cf969588d35deb335f4c734
Dhruv Bansal <shrub.vandal@gmail.com>
2017-05-25 16:42:01 -0500

    Whoops, forgot to update bohr version for new tag

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bohr "0.1.0"
+(defproject bohr "0.2.1"
   :description "Lightweight, extensible, metrics gathering and forwarding framework."
   :url "http://github.com/dhruvbansal/bohr"
   :license {:name "Apache Public License (2.0)"
